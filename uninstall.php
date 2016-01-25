b7cbc97709abf51dc50a712db5f55f2b60bdb280
Nicolas Lemoine <nico.lemoine@gmail.com>
2016-01-25 18:33:15 +0100

    Base for rewrite

--- uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( !defined('WP_UNINSTALL_PLUGIN') ) {
-    exit();
-}
-delete_option('wpbe_options');
-
-?>
---
+++ wp-better-emails.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: WP Better Emails
+ * Version: 0.9
+ * Plugin URI: http://wordpress.org/extend/plugins/wp-better-emails/
+ * Description: Beautify the default text/plain WordPress emails into fully customizable HTML emails.
+ * Author: Nicolas Lemoine
+ * Author URI: http://wordpress.org/extend/plugins/wp-better-emails/
+ * Text Domain: wp-better-emails
+ * Domain Path: /languages/
+ * License: GPL v3
+ */
